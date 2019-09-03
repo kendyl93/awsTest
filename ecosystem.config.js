b332ab4feb60bfa826ecc9869e99d7da4d88e433
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-09-03 19:14:46 +0200

    Added excosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'awsTest',
+      script: './index.js'
+    }
+  ],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-216-56-193.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/kendyl93.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/kendyl93/awsTest.git',
+      path: '/home/ubuntu/awsTest',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+};
